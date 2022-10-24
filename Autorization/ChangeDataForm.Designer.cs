58cbe9ad2d30c0e64ffa0997bbfaac8ced9ea50f
Максим Щукин <oleg-14317@mail.ru>
2022-10-24 09:45:56 +0500

    ficksiruyu

--- Autorization/ChangeDataForm.Designer.cs
+++ Autorization/ChangeDataForm.Designer.cs
@@ -36,14 +36,18 @@ namespace Autorization
             this.изПрограммыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.изАккаунтаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.вГлавноеМенюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridViewTransformData2 = new System.Windows.Forms.DataGridView();
+            this.TransformDataLabel = new System.Windows.Forms.Label();
             this.panel4.SuspendLayout();
             this.panel5.SuspendLayout();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTransformData2)).BeginInit();
             this.SuspendLayout();
             // 
             // panel4
             // 
             this.panel4.BackColor = System.Drawing.Color.Lavender;
+            this.panel4.Controls.Add(this.dataGridViewTransformData2);
             this.panel4.Controls.Add(this.panel5);
             this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel4.Location = new System.Drawing.Point(0, 0);
@@ -56,6 +60,7 @@ namespace Autorization
             // panel5
             // 
             this.panel5.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.panel5.Controls.Add(this.TransformDataLabel);
             this.panel5.Controls.Add(this.menuStrip1);
             this.panel5.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
@@ -107,12 +112,34 @@ namespace Autorization
             this.вГлавноеМенюToolStripMenuItem.Text = "В главное меню";
             this.вГлавноеМенюToolStripMenuItem.Click += new System.EventHandler(this.вГлавноеМенюToolStripMenuItem_Click);
             // 
+            // dataGridViewTransformData2
+            // 
+            this.dataGridViewTransformData2.BackgroundColor = System.Drawing.Color.Lavender;
+            this.dataGridViewTransformData2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridViewTransformData2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTransformData2.Location = new System.Drawing.Point(3, 106);
+            this.dataGridViewTransformData2.Name = "dataGridViewTransformData2";
+            this.dataGridViewTransformData2.Size = new System.Drawing.Size(716, 310);
+            this.dataGridViewTransformData2.TabIndex = 6;
+            // 
+            // TransformDataLabel
+            // 
+            this.TransformDataLabel.AutoSize = true;
+            this.TransformDataLabel.Font = new System.Drawing.Font("Mongolian Baiti", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TransformDataLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.TransformDataLabel.Location = new System.Drawing.Point(218, 38);
+            this.TransformDataLabel.Name = "TransformDataLabel";
+            this.TransformDataLabel.Size = new System.Drawing.Size(677, 50);
+            this.TransformDataLabel.TabIndex = 4;
+            this.TransformDataLabel.Text = "ФОРМА ИЗМЕНЕНИЯ ДАННЫХ";
+            // 
             // ChangeDataForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1102, 535);
             this.Controls.Add(this.panel4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "ChangeDataForm";
             this.Text = "ChangeDataForm";
@@ -121,6 +148,7 @@ namespace Autorization
             this.panel5.PerformLayout();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTransformData2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -134,5 +162,7 @@ namespace Autorization
         private System.Windows.Forms.ToolStripMenuItem изПрограммыToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem изАккаунтаToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem вГлавноеМенюToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridViewTransformData2;
+        private System.Windows.Forms.Label TransformDataLabel;
     }
 }
